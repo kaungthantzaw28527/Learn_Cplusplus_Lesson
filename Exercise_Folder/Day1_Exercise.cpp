7ae6acc7b25bd1093afe7b4997623f3d6450871d
kaungthantzaw28527 <kaungthantzaw28527@gmail.com>
2026-09-25 14:17:19 +0630

    Add Exercise_Folder

---
+++ Exercise_Folder/Day1_Exercise.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"--------------------------"<<endl;//endl means "End Line" and it is used when you want to start a new line
+	cout<<"My name is Kaung Thant Zaw"<<endl;
+	cout<<"I am a student."<<endl;
+	count<<"I have a girlfriend"<<endl;
+}
